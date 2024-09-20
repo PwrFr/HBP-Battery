3d413ec5df36fd57a7574be994df821388eb49a8
PwrFr <farrocker@Outlook.co.th>
2024-09-21 02:08:54 +0700

    fix: no repeat

--- src/App.vue
+++ src/App.vue
@@ -86,7 +86,7 @@ const handleClick = () => {
         </div>
     </div>
 
-    <div class="h-[100dvh] bg-slate-700 bg-fixed bg-phone bg-center sm:hidden block relative">
+    <div class="h-[100dvh]  bg-slate-700 bg-fixed bg-no-repeat bg-phone sm:hidden block relative">
 
         <div class="backdrop-blur-[2px] w-full h-full relative ">
 
